d06402b8187c09850cde15c861981167b7048bc9
cokkto <cocteau@live.fi>
2018-01-20 20:46:14 +0200

    remade header

--- themes/ooods/static/js/agency.js
+++ themes/ooods/static/js/agency.js
@@ -1,57 +1,71 @@
 // Smooth scrolling via animate()
-$(document).ready(function(){
-  $("a").on('click', function(event) {
+$(document).ready(function() {
+  $("a").on("click", function(event) {
     if (this.hash && window.location.pathname == "/") {
       event.preventDefault();
       var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
     }
   });
 });
 
 // Navigation change on scroll
-$(document).ready(function(){
+$(document).ready(function() {
   var maxOffset = 125;
   $(window).scroll(function() {
     if ($(window).scrollTop() >= maxOffset) {
-      $('.navbar-default').addClass('navbar-shrink');
-    }
-    else {
-      $('.navbar-default').removeClass('navbar-shrink');
+      $("nav.navbar").addClass("navbar-shrink");
+    } else {
+      $("nav.navbar").removeClass("navbar-shrink");
     }
   });
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+// $('body').scrollspy({
+//     target: '.navbar-fixed-top'
+// })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+// $('.navbar-collapse ul li a').click(function() {
+//     $('.navbar-toggle:visible').click();
+// });
+$(document).ready(function() {
+  $("button.navbar-toggle").click(function() {
+    $("ul.nav").toggleClass("hidden");
+  });
 });
 
 // Async contact form
-$('form[id=contactForm]').submit(function(){
-  $.post($(this).attr('action'), $(this).serialize(), function(data, textStatus, jqXHR){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (jqXHR.status == 200) {
-      $('form[id=contactForm] #success').show();
-    }}, 'json').fail(function(){
-      $('form[id=contactForm] #success').hide();
-      $('form[id=contactForm] #error').hide();
-      $('form[id=contactForm] #error').show();
+$("form[id=contactForm]").submit(function() {
+  $.post(
+    $(this).attr("action"),
+    $(this).serialize(),
+    function(data, textStatus, jqXHR) {
+      $("form[id=contactForm] #success").hide();
+      $("form[id=contactForm] #error").hide();
+      if (jqXHR.status == 200) {
+        $("form[id=contactForm] #success").show();
+      }
+    },
+    "json"
+  ).fail(function() {
+    $("form[id=contactForm] #success").hide();
+    $("form[id=contactForm] #error").hide();
+    $("form[id=contactForm] #error").show();
   });
   return false;
 });
 
 // Contact form validation
 $.validate({
-  modules : 'html5, toggleDisabled'
+  modules: "html5, toggleDisabled"
 });
